290483443dd7f5daf782d40ce227a183d376275a
Andre Eppinghaus <andreeppinghaus@gmail.com>
2025-03-24 21:01:15 -0300

    versao com o dockeroperator

--- example_apache_airflow/dags/containers1_dag.py
+++ example_apache_airflow/dags/containers1_dag.py
@@ -1,27 +1,43 @@
-
 from datetime import datetime, timedelta
-
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 
 default_args = {
     'owner': 'andre',
     'retries': 5,
     'retry_delay': timedelta(minutes=2)
 }
+
 with DAG(
-    dag_id = "containers1_dag",
-    default_args = default_args,
-    description = "Execucação de containers em PHP, atualizando um arquivo",
+    dag_id="containers1_dag",
+    default_args=default_args,
+    description="Execução de containers em PHP, atualizando um arquivo",
     start_date=datetime(2024, 3, 1),
     schedule_interval='@daily',
 ) as dag:
-    task1 = BashOperator(
-        task_id ='php_app1',
-        bash_command='docker run -v $(pwd)/../in:/usr/src/app/in -v $(pwd):/usr/src/app php-app1'
+    
+    task1 = DockerOperator(
+        task_id='php_app1',
+        image='php-app1',  # Nome da sua imagem Docker
+        api_version='auto',
+        auto_remove=True,
+        docker_url='unix://var/run/docker.sock',
+        volumes=[
+            '$(pwd)/../in:/usr/src/app/in',
+            '$(pwd):/usr/src/app'
+        ],
+        network_mode='bridge'
     )
 
-    task2 = BashOperator(
-        task_id ='php_app2',
-        bash_command='docker run -v $(pwd)/../in:/usr/src/app/in -v $(pwd):/usr/src/app php-app2'
+    task2 = DockerOperator(
+        task_id='php_app2',
+        image='php-app2',  # Nome da sua imagem Docker
+        api_version='auto',
+        auto_remove=True,
+        docker_url='unix://var/run/docker.sock',
+        volumes=[
+            '$(pwd)/../in:/usr/src/app/in',
+            '$(pwd):/usr/src/app'
+        ],
+        network_mode='bridge'
     )
